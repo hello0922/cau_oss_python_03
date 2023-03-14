cbb742e8353c9b53497de1df1dd47a6f13df4e0a
hello0922 <hello0922@cau.ac.kr>
2023-03-14 16:01:10 +0900

    Create prac1.py
    2주차 실습, 점수합산 예제 생성

---
+++ prac1.py
@@ -0,0 +1,8 @@
+hw_score = input("hw")
+att_score = input("att")
+mid_score = input("mid")
+final_score = input("final")
+
+total_score = hw_score + att_score + mid_score + final_score
+
+print("Total Score is", total_score)
